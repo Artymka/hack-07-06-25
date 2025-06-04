67a2c10041f785b0f6b9050dcaa211143a1a93ac
Artymka <doomparser@gmail.com>
2025-06-04 22:22:54 +0300

    Add sixth file

--- dataset/convert.py
+++ dataset/convert.py
@@ -38,3 +38,8 @@ engine = create_engine(url)
 for file, table in zip(files, tables):
     df = pd.read_parquet(file, engine="fastparquet")
     df.to_sql(table, engine, if_exists="replace", index=False)
+
+excel_file = "t_dict_dict_municipal_districts.xlsx"
+table = "municipal_districts"
+df = pd.read_excel(excel_file, sheet_name="Sheet1")
+df.to_sql(table, engine, if_exists="replace", index=False)
